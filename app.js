3898777873aa3764a6266e72d2d5efa6f629bcf4
Ruka <98132136+LukeT8@users.noreply.github.com>
2023-10-02 17:42:36 +1000

    add views back for now

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var helmet = require("helmet");
 
 var app = express();
 
-// view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "pug");
+//view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
